6f1d2ce663dceb973002841bb7c7d6f15fa244e4
Oussama Bentalha <oussama.bentalha@gmail.com>
2017-01-09 01:38:29 +0100

    Play music online Test

--- iMusicTests/iMusicPlayerTests.swift
+++ iMusicTests/iMusicPlayerTests.swift
@@ -7,22 +7,20 @@
 //
 
 import XCTest
+@testable import iMusic
 
 class iMusicPlayerTests: XCTestCase {
     
+    var musicViewController : MusicViewController?
+    
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        self.musicViewController = MusicViewController(nibName: "MusicViewController", bundle: nil)
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
     
     func testPerformanceExample() {
@@ -31,5 +29,13 @@ class iMusicPlayerTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
+    
+    func testA(){
+        //Play music online
+        musicViewController!.launchMusicWithId(musicId: "583c186c21cb3a100df74f38")
+    }
+    
+    
+    
     
 }
